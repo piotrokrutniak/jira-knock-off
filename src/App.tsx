2c59e7086ae841ef37940ac30899d54fcc0bf596
piotrokrutniak <91792866+piotrokrutniak@users.noreply.github.com>
2024-06-22 19:10:07 +0200

    Prettier

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,7 @@
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import "./App.css";
 
-const queryClient = new QueryClient()
+const queryClient = new QueryClient();
 
 function App() {
   return (
