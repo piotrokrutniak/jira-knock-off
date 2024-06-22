003317f0f884ab3f5a75af627cc2832d20d71cc5
piotrokrutniak <91792866+piotrokrutniak@users.noreply.github.com>
2024-06-22 18:40:27 +0200

    react query

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,14 @@
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import "./App.css";
+
+const queryClient = new QueryClient()
 
 function App() {
   return (
     <>
+      <QueryClientProvider client={queryClient}>
+        
+      </QueryClientProvider>
     </>
   );
 }
