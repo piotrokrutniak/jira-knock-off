7bb59b82cfd04f2e3a62b01a605b3cadc44c4420
Piotr Okrutniak <piotrokrutniak@gmail.com>
2024-06-23 20:23:39 +0200

    Types directory

--- src/components/feature/dashboard/ProjectSelector.tsx
+++ src/components/feature/dashboard/ProjectSelector.tsx
@@ -2,14 +2,15 @@ import clsx from "clsx";
 import { useState } from "react";
 import { FaPlus } from "react-icons/fa";
 import { Input } from "@components/ui/input";
+import { ProjectType } from "@customtypes/types";
 
 export const ProjectSelector = () => {
   const [projects, setProjects] = useState([]) // [Project, Project, Project
   return (
     <section id="projects-selector">
-      <div className="flex justify-between mb-8">
-        <h1 className="text-3xl">Select Project</h1>
-        <Input placeholder="Search Projects" />
+      <div className="flex justify-between mb-8 gap-8 max-sm:flex-col">
+        <h1 className="text-3xl font-semibold text-nowrap">Select Project</h1>
+        <Input className="sm:max-w-96" placeholder="Search Projects" />
       </div>
       {/* <ProjectPanel /> */}
       {projects.length
@@ -25,17 +26,22 @@ export const ProjectSelector = () => {
   )
 };
 
-const ProjectPanel = () => {
+const ProjectPanel = (project: ProjectType) => {
+  // display project panel data
   return (
-    <></>
+    <div className={clsx([
+      "p-16 aspect-square rounded-lg border-2 flex place-content-center justify-center items-center",
+      "shadow-sm hover:shadow-md transition-all cursor-pointer hover:bg-slate-200/5",
+    ])}>Project name</div>
   )
 };
 
 const AddProjectPanel = () => {
+  // navigate to create project page
   return (
     <div className={clsx([
       "p-16 aspect-square rounded-lg border-2 flex place-content-center justify-center items-center",
-      "shadow-sm hover:shadow-md transition-shadow cursor-pointer",
+      "shadow-sm hover:shadow-md transition-all cursor-pointer hover:bg-slate-200/5",
     ])}><FaPlus className="flex" /></div>
   )
 };
--- src/components/feature/navbar/Navbar.tsx
+++ src/components/feature/navbar/Navbar.tsx
@@ -4,15 +4,15 @@ import { ThemeSwitcher } from "./ThemeSwitcher";
 export const Navbar = () => {
   return (
     <div className="p-2  bg-blue-500 text-white w-full shadow-sm shadow-black">
-      <div className="max-w-5xl flex justify-between gap-2 m-auto">
+      <div className="max-w-5xl flex justify-between gap-2 m-auto place-items-center">
       <div>
         <span className="font-bold">Jira</span>
       </div>
       <div className="flex gap-2">
-        <Link to="/" className="[&.active]:font-bold">
+        <Link to="/" className="[&.active]:font-bold place-items-center flex">
           Home
         </Link>
-        <Link to="/about" className="[&.active]:font-bold">
+        <Link to="/about" className="[&.active]:font-bold place-items-center flex">
           About
         </Link>
         <ThemeSwitcher />
