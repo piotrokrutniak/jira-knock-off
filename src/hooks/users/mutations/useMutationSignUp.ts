90602d9a6a820da1b811968cafa69fdc08b22ede
Piotr Okrutniak <piotrokrutniak@gmail.com>
2024-06-30 16:38:08 +0200

    dotenv, mutations

---
+++ src/hooks/users/mutations/useMutationSignUp.ts
@@ -0,0 +1,12 @@
+import { useMutation } from "@tanstack/react-query";
+
+export const useMutationSignUp = () => {
+  // return useMutation({
+  //   mutationFn: 
+  // });
+};
+
+const signUp = () => {
+  // sign up user
+  // return user
+};
